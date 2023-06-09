548eb1c1a0d8a1055e14fba14e141ebed85093fa
jyking <jyking@qq.com>
2023-06-09 08:08:28 +0800

    feat: 添加登录页面
    添加了一个新的登录页面，路径为 `/login`，组件名为 `login`。在该页面中使用了 Vue 3 的 Composition API，并在生命周期钩子函数中打印了一些信息。
    
    修改了 `tsconfig.json` 文件，将 `include` 属性设置为 `"src/**/*.ts", "src/**/*.d.ts", "src/**/*.tsx", "src/**/*.vue"`。
    
    更新了百度统计代码，将变量 `_hmt` 改为常量 `const _hmt`。

--- src/router/index.ts
+++ src/router/index.ts
@@ -19,6 +19,12 @@ const routes: RouteRecordRaw[] = [
     ],
   },
 
+	{
+		path: '/login',
+		name: 'login',
+		component: () => import('@/views/login/index.vue'),
+	},
+
   {
     path: '/404',
     name: '404',
---
+++ src/views/login/index.vue
@@ -0,0 +1,17 @@
+<script setup lang='ts'>
+import { onMounted, onUnmounted } from 'vue'
+
+onMounted(() => {
+  console.log('onMounted')
+})
+
+onUnmounted(() => {
+  console.log('onUnmounted')
+})
+</script>
+
+<template>
+  <div>
+    login222
+  </div>
+</template>
